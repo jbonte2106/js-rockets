ae0dbba4d4138069028cfd234a731270a5466ce8
jbonte2106 <34454089+jbonte2106@users.noreply.github.com>
2017-12-12 17:10:28 -0500

    Edits

--- JS/Script.js
+++ JS/Script.js
@@ -1,43 +1,15 @@
-<DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 	<title>JS Rockets</title>
 	<script type="text/javascript">
-		var sayMyName = function (name) {
-			alert('My name is: ' +name);
-		}
-		var car = {
-			make: 'VW',
-			type: 'Polo',
-			colour: 'Blue',
-			isTurnedOn: false,
-			numberOfWheels: 4,
-			seats:[
-				'seat1',
-				'seat2',
-				'seat3',
-				'seat4'
-			],
-			turnOn: function () {
-				this.isTurnedOn = true;
-			},
-			fly: function () {
-				alert ('fly')
-			},
-			switchCar:function (isOn) {
-				console.log ('turn car' + isOn)
-				if (isOn == true) {
-					this.isTurnedOn = true;
-				} else {
-					this.isTurnedOn = false;
-				}
-			}
-		};		
-	console.log('hello there friends!');
+		
 </script>
 </head>
 <body>
-
+	var sayMyName = function (name) {
+			alert('My name is: ' +name);
+	}
 </body>
 <h1>Hello JS Rockets</h1>
 </html>
