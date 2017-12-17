7b8a179ee7fcd471333d81511723848042903a4e
jbonte2106 <34454089+jbonte2106@users.noreply.github.com>
2017-12-16 21:05:39 -0500

    .

--- JS/rocket.js
+++ JS/rocket.js
@@ -1,8 +1,14 @@
+var timer = null;
+
 var changeState = function  (state) {
 	document.body.className = 'body-state'+
 		state; 
 
 		if (state == 2) {
+			timer = setInterval(function ()  {
+
+		}, 1000);
 			document.getElementById('countdown')
-		};
-}
+				innerHTML = 9;
+		;
+}}
